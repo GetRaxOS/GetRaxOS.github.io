ba821dac36ee91de336e354bee150151660d802f
GetRaxOS <marekkucharek133@gmail.com>
2024-09-30 14:58:43 +0200

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        title: "FPS Improvements",
+        description: "We've tested performance on Fortnite and VALORANT. Here are the results: VALORANT: Stock Windows: 217.5 FPS | Rax>: 365.95 FPS | FORTNITE: Stock Windows: 98.51 FPS | Rax>: 253.98 FPS",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Less background processes",
+        description: "Stock Windows: 140+ | Rax>: 70. This helps with the RAM usage. Less tasks =  more RAM available to allocate to other tasks, like games.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "No preinstalled apps",
+        description: "Rax> removes most, if not all of the applications installed via Microsoft Store. This includes things like Notepad, Sniping tool, etc. Every app can be reinstalled after installing Rax>.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Removed Windows Defender",
+        description: "Oh yes. Windows Offender, the most annoying thing known on earth. If you really feel like you need an antivirus, use Kaspersky Free.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "More consistent dark mode",
+        description: "Rax makes things like the WIN + R Dialog (Run) Dark themed. This also applies but is not just properties in the file explorer and the control panel.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "If you face any issues, join our Discord server and ask for help!",
+        description: "Some members will be able to help you. Please don't ping anyone, if somebody knows a solution to your issue, they'll most likely let you know about it. Patience is key here.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
